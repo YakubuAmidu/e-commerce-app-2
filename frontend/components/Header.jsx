8cbed9dcbf7a9706b2557a2f1cfaf05490dad23b
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-03-18 14:12:19 -0700

    Initial commit

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,16 +1,45 @@
 import React, { Fragment } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
 import { Avatar } from "react-native-paper";
 import { colors } from "../styles/styles";
 
-const Header = () => {
+const Header = ({ back, emptyCart = false }) => {
+  const navigate = useNavigation();
+  const route = useRoute();
+
+  const emptyCartHandler = () => {
+    console.log("EmptyCartHandler");
+  };
+
   return (
     <Fragment>
-      <TouchableOpacity style={{ position: "absolute" }}>
+      {back && (
+        <TouchableOpacity
+          style={{ position: "absolute", left: 20, top: 40, zIndex: 10 }}
+          onPress={() => navigate.goBack()}
+        >
+          <Avatar.Icon
+            style={{ backgroundColor: colors.color4 }}
+            icon={"arrow-left"}
+            color={
+              route.name === "productdetails" ? colors.color2 : colors.color3
+            }
+          />
+        </TouchableOpacity>
+      )}
+
+      <TouchableOpacity
+        style={{ position: "absolute", right: 20, top: 40, zIndex: 10 }}
+        onPress={emptyCart ? emptyCartHandler : () => navigate.navigate("Cart")}
+      >
         <Avatar.Icon
           style={{ backgroundColor: colors.color4 }}
-          icon={"arrow-left"}
-          color={colors.color3}
+          icon={emptyCart ? "delete-outline" : "cart-outline"}
+          color={
+            route.name === "productdetails" ? colors.color2 : colors.color3
+          }
         />
       </TouchableOpacity>
     </Fragment>
