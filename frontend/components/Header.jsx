c0445a39863df0cd5789d1363b8c613c67d64470
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-03-18 11:19:53 -0700

    Initial commit

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { Fragment } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Avatar } from "react-native-paper";
+import { colors } from "../styles/styles";
 
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
-    </View>
-  )
-}
+    <Fragment>
+      <TouchableOpacity style={{ position: "absolute" }}>
+        <Avatar.Icon
+          style={{ backgroundColor: colors.color4 }}
+          icon={"arrow-left"}
+          color={colors.color3}
+        />
+      </TouchableOpacity>
+    </Fragment>
+  );
+};
 
-export default Header
+export default Header;
