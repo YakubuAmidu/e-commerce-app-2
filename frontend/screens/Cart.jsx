192cf22f08f9f5b64733059a8b2a78ead2eece3c
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-03-27 20:02:31 -0700

    Initial commit

--- frontend/screens/Cart.jsx
+++ frontend/screens/Cart.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import { defaultStyle } from "../styles/styles";
+import Header from "../components/Header.jsx";
+
 const Cart = () => {
   return (
-    <View>
+    <View style={{ ...defaultStyle, padding: 0 }}>
+      {/*Header*/}
+      <Header back={true} emptyCart={true} />
+
+      {/*Heading*/}
+      <Heading />
       <Text>Cart...</Text>
     </View>
   );
 };
+
+const Heading = () => {};
 
 export default Cart;
