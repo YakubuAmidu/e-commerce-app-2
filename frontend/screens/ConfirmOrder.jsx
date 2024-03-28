d5577a80482fcbdc3934643b5e43fbda7533836f
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-03-28 09:18:57 -0700

    Initial commit

--- frontend/screens/ConfirmOrder.jsx
+++ frontend/screens/ConfirmOrder.jsx
@@ -1,14 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 
 import Header from "../components/Header.jsx";
+import ConfirmOrderItem from "../components/ConfirmOrderItem.jsx";
 import { defaultStyle } from "../styles/styles.js";
+import { cartItems } from "./Cart";
 
 const ConfirmOrder = () => {
   return (
     <View style={defaultStyle}>
       <Header back={true} />
-      <Text>ConfirmOrder</Text>
+
+      {/*Heading*/}
+      <Heading
+        containerStyle={{ paddingTop: 70 }}
+        text1="cofirm"
+        text2="Order"
+      />
+
+      <View style={{ paddingVertical: 20, flexx: 1 }}>
+        <ScrollView>
+          {cartItems.map((item, index) => (
+            <ConfirmOrderItem />
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 };
