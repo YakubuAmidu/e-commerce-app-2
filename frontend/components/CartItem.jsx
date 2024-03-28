d0717e3cde97dd90edae2e494fccd301a77b5e40
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-03-27 21:33:10 -0700

    Initial commit

--- frontend/components/CartItem.jsx
+++ frontend/components/CartItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 const CartItem = ({
   name,
@@ -12,8 +12,21 @@ const CartItem = ({
   imgSrc,
 }) => {
   return (
-    <View>
-      <Text>CartItems</Text>
+    <View style={{ flexDirection: "row", heitght: 100, marginVertical: 20 }}>
+      <View
+        style={{
+          width: "40%",
+          backgroundColor: index % 2 === 0 ? colors.color1 : colors.color3,
+          borderTopRightRadius: 100,
+          borderBottomRightRadius: 100,
+        }}
+      >
+        <Image source={{ uri: imgSrc }} style={{ width: 200, heitght: '100%', resizeMode="contain" top: '-20%', left: '10%' }} />
+      </View>
+
+      <View style={{ width: '40%', paddingHorizontal: 25, }}>
+        <Text numberOfLines={1} style={{ fontSize: 17 }}>{name}</Text>
+      </View>
     </View>
   );
 };
