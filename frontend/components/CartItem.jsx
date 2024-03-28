2a49ce69c8ce71fe7f0b0bdea3dcb64d69b494f2
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-03-28 00:49:13 -0700

    Initial commit

--- frontend/components/CartItem.jsx
+++ frontend/components/CartItem.jsx
@@ -44,35 +44,37 @@ const CartItem = ({
         </Text>
       </View>
 
-      <View
-        style={{
-          alignItems: "center",
-          justifyContent: "center",
-          alignSelf: "center",
-          width: "20%",
-          height: 80,
-        }}
-      >
-        <TouchableOpacity onPress={decrementHandler(id, qty)}>
+      <View style={qtyContainer}>
+        <TouchableOpacity onPress={() => decrementHandler(id, qty)}>
           <Avatar.Icon icon={"minus"} {...iconOptions} />
-          <Text
-            style={{
-              backgroundColor: colors.color4,
-              height: 25,
-              width: 25,
-              textAlignVertical: "center",
-              textAlign: "center",
-              borderWidth: 1,
-              borderRadius: 5,
-              borderColor: colors.color5,
-            }}
-          >
-            {qty}
-          </Text>
+        </TouchableOpacity>
+        <Text style={styles.qtyText}>{qty}</Text>
+        <TouchableOpacity onPress={() => incrementHandler(id, qty, stock)}>
+          <Avatar.Icon icon={"plus"} {...iconOptions} />
         </TouchableOpacity>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  qtyText: {
+    backgroundColor: colors.color4,
+    height: 25,
+    width: 25,
+    textAlignVertical: "center",
+    textAlign: "center",
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: colors.color5,
+  },
+  qtyContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+    width: "20%",
+    height: 80,
+  },
+});
 
 export default CartItem;
