8db9381d33c4559b5113784f38f439edeea5c2e0
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-04-01 21:59:14 -0700

    Initial commit

--- frontend/components/ConfirmOrderItem.jsx
+++ frontend/components/ConfirmOrderItem.jsx
@@ -1,10 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 
-const ConfirmOrderItem = () => {
+const ConfirmOrderItem = ({ quantity, price, image, name }) => {
   return (
-    <View>
-      <Text>ConfirmOrderItem</Text>
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: 10,
+      }}
+    >
+      <Image
+        source={{
+          uri: image,
+        }}
+        style={{ width: 50, height: 50, resizeMode: "contain" }}
+      />
+      <Text>{name}</Text>
     </View>
   );
 };
