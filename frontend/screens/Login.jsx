1fd56e77406d01237290acc0a5535d2c46c08edf
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-04-03 09:38:50 -0700

    Initial commit

--- frontend/screens/Login.jsx
+++ frontend/screens/Login.jsx
@@ -1,12 +1,47 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { TextInput } from "react-native-paper";
+
+import { defaultStyle, colors, inputStyling } from "../styles/styles";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={defaultStyle}>
+      {/* Heading */}
+      <View style={{ marginBottom: 20 }}>
+        <Text style={styles.heading}>Login</Text>
+      </View>
+      <View style={styles.container}>
+        <TextInput
+          style={inputStyling}
+          mode={"outlined"}
+          activeOutlineColor={colors.color1}
+        />
+      </View>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 25,
+    fontWeight: 600,
+    textAlign: "center",
+    backgroundColor: colors.color3,
+    color: colors.color2,
+    padding: 5,
+    borderRadius: 5,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: colors.color3,
+    borderRadius: 10,
+    elevation: 10,
+    justifyContent: "center",
+  },
+});
 export default Login;
