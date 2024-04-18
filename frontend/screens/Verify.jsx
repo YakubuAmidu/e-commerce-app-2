e07e3412d7a19a3ded046caf12c701d595c19c5b
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-04-17 22:16:22 -0700

    Initial commit

--- frontend/screens/Verify.jsx
+++ frontend/screens/Verify.jsx
@@ -1,11 +1,66 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState, Fragment } from "react";
+import { TextInput, Button } from "react-native-paper";
 
-const Verify = () => {
+import {
+  defaultStyle,
+  colors,
+  FormHeading,
+  inputOptions,
+  formStyles as styles,
+} from "../styles/styles";
+
+const Verify = ({ navigation }) => {
+  const [otp, setOtp] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loading = false;
+
+  const submitHandler = () => {
+    alert("Yeah");
+    // Will remove this in the future.
+    navigation.navigate("login");
+  };
+
   return (
-    <View>
-      <Text>Verify</Text>
-    </View>
+    <Fragment>
+      <View style={defaultStyle}>
+        {/* Heading */}
+        <View style={{ marginBottom: 20 }}>
+          <Text style={FormHeading}>Reset Password</Text>
+        </View>
+        <View style={styles.container}>
+          <TextInput
+            {...inputOptions}
+            placeholder="OTP"
+            keyboardType="number-pad"
+            value={otp}
+            onChangeText={setOtp}
+          />
+
+          <Button
+            loading={loading}
+            textColor={colors.color2}
+            style={styles.btn}
+            disabled={email === ""}
+            onPress={submitHandler}
+          >
+            Send OTP
+          </Button>
+
+          <Text style={styles.or}>OR</Text>
+
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() => navigation.navigate("login")}
+          >
+            <Text style={styles.link}>Log In</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <Footer activeRoute="profile" />
+    </Fragment>
   );
 };
 
