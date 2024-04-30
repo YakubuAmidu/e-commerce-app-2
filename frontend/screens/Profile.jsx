f961dac1ce526fc6c11e1ceea6f8c9a77032d67a
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-04-29 20:46:49 -0700

    Initial commit

--- frontend/screens/Profile.jsx
+++ frontend/screens/Profile.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { defautStyle, formHeading } from "../styles/styles";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile page</Text>
+    <View style={defautStyle}>
+      {/* Heading */}
+      <View style={{ marginBottom: 20 }}>
+        <Text style={formHeading}>Profile</Text>
+      </View>
+      {/* Loading */}
+      <View style={styles.container}></View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    elevation: 7,
+    backgroundColor: colors.color3,
+    padding: 30,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+});
 
 export default Profile;
