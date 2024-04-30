3f83570dc47226848975f6818b89da92cef18ab6
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-04-29 20:55:07 -0700

    Initial commit

--- frontend/screens/Profile.jsx
+++ frontend/screens/Profile.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { defautStyle, formHeading } from "../styles/styles";
+import { Avatar } from "react-native-paper";
 
 const Profile = () => {
+  const [avatar, setAvatar] = useState("");
+
   return (
     <View style={defautStyle}>
       {/* Heading */}
@@ -10,7 +13,15 @@ const Profile = () => {
         <Text style={formHeading}>Profile</Text>
       </View>
       {/* Loading */}
-      <View style={styles.container}></View>
+      <View style={styles.container}>
+        <Avatar.Image
+          source={{
+            uri: avatar,
+          }}
+          size={100}
+          style={{ backgroundColor: colors.color1 }}
+        />
+      </View>
     </View>
   );
 };
